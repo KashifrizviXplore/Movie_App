458bb8fca1f8dd27c6438d76854bf029c2c8e64b
KashifrizviXplore <kashifrizvi339@gmail.com>
2025-11-13 23:46:27 -0800

    add pagiantion feature / component

--- src/api/yts.js
+++ src/api/yts.js
@@ -2,14 +2,21 @@ import axios from "axios";
 
 const BASE_URL = "https://yts.mx/api/v2/";
 
-export const getMovies = async (query = "", genre = "") => {
+export const getMovies = async (query = "", genre = "", page = 1) => {
   const res = await axios.get(`${BASE_URL}list_movies.json`, {
     params: {
       query_term: query,
       genre: genre,
+      page: page,
+      limit: 20, // optional
     },
   });
-  return res.data.data.movies || [];
+
+  return {
+    movies: res.data.data.movies || [],
+    total: res.data.data.movie_count,
+    limit: res.data.data.limit,
+  };
 };
 
 export const getMovieDetails = async (id) => {
